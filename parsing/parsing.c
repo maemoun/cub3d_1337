a287c8673c8f88143e2c1431061c05fd02ef3896
Maemoun Skhairi <maeskhai@c1r3p9.1337.ma>
2025-09-24 15:12:53 +0100

    cub2

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: maeskhai <maeskhai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 11:07:16 by maeskhai          #+#    #+#             */
-/*   Updated: 2025/09/23 13:29:43 by maeskhai         ###   ########.fr       */
+/*   Updated: 2025/09/24 15:11:50 by maeskhai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,15 @@
 
 void    ft_parsing(char *fd_in, t_list *list)
 {
+    int i = 0;
 	ft_check_file_name(fd_in);
 	ft_read_fd(fd_in, list);
-    int i = 0;
-    while (1)
+    if (!list->fd_lines)
+        ft_print_error("Error\nMap reading failed!\n", NULL, NULL);
+    while (list->fd_lines[i])
     {
         printf("%s\n", list->fd_lines[i]);
+        i++;
     }
 	// ft_check_map_errors(&list);
 }
